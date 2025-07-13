76068f18f343e1d09e4862429b4949ffd3b72b4d
miguel-willians <miiguel.wso@gmail.com>
2025-07-13 17:12:22 -0300

    feat: implement API to fetch cabins

--- app/_components/CabinCard.tsx
+++ app/_components/CabinCard.tsx
@@ -1,15 +1,25 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { Cabin } from "../_types/cabin";
 
-function CabinCard({ cabin }) {
+interface CabinCardProps {
+  cabin: Cabin;
+}
+
+function CabinCard({ cabin }: CabinCardProps) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
     <div className="flex border-primary-800 border">
-      <img
-        src={image}
-        alt={`Cabin ${name}`}
-        className="flex-1 border-r border-primary-800"
-      />
+      <div className="relative aspect-square flex-1">
+        <Image
+          fill
+          src={image}
+          alt={`Cabin ${name}`}
+          className="flex-1 border-r border-primary-800 object-cover"
+        />
+      </div>
 
       <div className="flex-grow">
         <div className="pt-5 pb-4 px-7 bg-primary-950">
@@ -42,12 +52,12 @@ function CabinCard({ cabin }) {
         </div>
 
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+          <Link
             href={`/cabins/${id}`}
             className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
