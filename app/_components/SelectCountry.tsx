21ff064b5c99d2f76c4ec7ddd5cf0c99fe4e3fed
miguel-willians <miiguel.wso@gmail.com>
2025-07-13 13:03:18 -0300

    feat: create basic structure for the profile page

---
+++ app/_components/SelectCountry.tsx
@@ -0,0 +1,27 @@
+export default async function SelectCountry({
+  defaultCountry,
+  name,
+  id,
+  className,
+}) {
+  const countries = [];
+
+  const flag =
+    countries.find((country) => country.name === defaultCountry)?.flag ?? "";
+
+  return (
+    <select
+      name={name}
+      id={id}
+      defaultValue={`${defaultCountry}%${flag}`}
+      className={className}
+    >
+      <option value="">Select country...</option>
+      {countries.map((c) => (
+        <option key={c.name} value={`${c.name}%${c.flag}`}>
+          {c.name}
+        </option>
+      ))}
+    </select>
+  );
+}
