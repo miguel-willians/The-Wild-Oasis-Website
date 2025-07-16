e6740a340df7d05f5d3b848d5cdfd1b33aaf028d
miguel-willians <miiguel.wso@gmail.com>
2025-07-16 16:12:08 -0300

    feat: set up error boundaries

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+interface ErrorComponentProps {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+export const metadata = {
+  title: "Something went wrong 😟",
+};
+
+export default function Error({ error, reset }: ErrorComponentProps) {
+  return (
+    <div className="flex justify-center items-center flex-col gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+      <p className="text-lg">{error.message}</p>
+
+      <button
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+        onClick={reset}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Not found 😟",
+};
+
+export default function NotFound() {
+  return (
+    <div className="text-center space-y-6 mt-4">
+      <h1 className="text-3xl font-semibold">
+        This page could not be found 😟
+      </h1>
+      <Link
+        href="/"
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+      >
+        Go back home
+      </Link>
+    </div>
+  );
+}
