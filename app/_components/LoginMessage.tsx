813087d58c8245b1baa074de27720591f351a50e
miguel-willians <miiguel.wso@gmail.com>
2025-07-23 13:44:48 -0300

    style: adjust styling

--- app/_components/LoginMessage.tsx
+++ app/_components/LoginMessage.tsx
@@ -1,6 +1,6 @@
 export default function LoginMessage() {
   return (
-    <div className="grid bg-primary-800 ">
+    <div className="grid bg-primary-800 h-full">
       <p className="text-center text-xl py-12 self-center">
         Please{" "}
         <a href="/login" className="underline text-accent-500">
