09adf60961fc6621678f21cea91556b73616f246
miguel-willians <miiguel.wso@gmail.com>
2025-08-08 15:55:51 -0300

    fix: adjust display text

--- app/_components/Cabin.tsx
+++ app/_components/Cabin.tsx
@@ -40,7 +40,7 @@ export default function Cabin({ cabin }: CabinProps) {
           <li className="flex gap-3 items-center">
             <MapPinIcon className="h-5 w-5 text-primary-600" />
             <span className="text-lg">
-              Located in the heart of the
+              Located in the heart of the{" "}
               <span className="font-bold">Dolomites</span> (Italy)
             </span>
           </li>
