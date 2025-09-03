11a7b972a1c66bafe381880176a19731ee66860c
miguel-willians <miiguel.wso@gmail.com>
2025-09-03 15:34:58 -0300

    fix: correct optimistic rendering upon reservation deletion

--- app/_components/ReservationList.tsx
+++ app/_components/ReservationList.tsx
@@ -4,6 +4,7 @@ import { useOptimistic } from "react";
 import { Booking } from "../_types/types";
 import ReservationCard from "./ReservationsCard";
 import { deleteReservation } from "../_lib/actions";
+import Link from "next/link";
 
 interface ReservationListProps {
   bookings: Booking[];
@@ -23,14 +24,25 @@ export default function ReservationList({ bookings }: ReservationListProps) {
   }
 
   return (
-    <ul className="space-y-6">
-      {optimisticBookings.map((booking) => (
-        <ReservationCard
-          booking={booking}
-          key={booking.id}
-          onDelete={handleDelete}
-        />
-      ))}
-    </ul>
+    <>
+      {optimisticBookings.length === 0 ? (
+        <p className="text-lg">
+          You have no reservations yet. Check out our{" "}
+          <Link className="underline text-accent-500" href="/cabins">
+            luxury cabins &rarr;
+          </Link>
+        </p>
+      ) : (
+        <ul className="space-y-6">
+          {optimisticBookings.map((booking) => (
+            <ReservationCard
+              booking={booking}
+              key={booking.id}
+              onDelete={handleDelete}
+            />
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
